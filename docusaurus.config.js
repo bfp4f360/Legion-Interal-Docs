ad9bc66bfc90b437f9fcf779788495ef76bde8af
Namenai <bfp4f360@gmail.com>
2020-05-20 01:55:10 -0400

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'SWLS Internal Test Doc',
-  tagline: 'Hello there crimmie boi 👋.',
+  tagline: 'Hello there crimmie boi 👋. Is this not cool!',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
